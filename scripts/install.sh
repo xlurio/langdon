1ead3788477b42e18f77917f154fd7e5b14e927c
Lucas Calegario <lucas.c.calegario@outlook.com>
2025-03-30 02:57:39 -0300

    feat: update install script to set PYENV environment variables in both .bashrc and .profile, and refresh shell after Python installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,11 +27,15 @@ curl -fsSL https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init - bash)"' >> ~/.profile
 exec "$SHELL"
 
 # Python
 pyenv install 3
 pyenv global 3
+exec "$SHELL"
 
 # Python packages
 python -m pip install --user dnsgen wafw00f
