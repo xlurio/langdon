22e516657834923e88aebe9b6c0ee3fea3c2869b
Lucas Calegario <lucas.c.calegario@outlook.com>
2025-03-30 04:32:30 -0300

    feat: update install script to include Go binary path in PATH variable and streamline package installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -54,20 +54,18 @@ cargo install apkeep
 # Go
 wget https://go.dev/dl/go1.24.1.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.1.linux-amd64.tar.gz
-echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
-echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.profile
+echo 'export PATH="$PATH:/usr/local/go/bin:'$HOME'/go/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:/usr/local/go/bin:'$HOME'/go/bin"' >> ~/.profile
 exec "$SHELL"
 
 # Go Packages
-go install \
-    github.com/tomnomnom/assetfinder@latest \
-    github.com/lc/gau/v2/cmd/gau@latest \
-    github.com/OJ/gobuster/v3@latest \
-    github.com/sensepost/gowitness@latest \
-    github.com/projectdiscovery/httpx/cmd/httpx@latest \
-    github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest \
-    github.com/owasp-amass/amass/v4/...@master
-
+go install github.com/tomnomnom/assetfinder@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/OJ/gobuster/v3@latest
+go install github.com/sensepost/gowitness@latest
+go install github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install github.com/owasp-amass/amass/v4/...@master
 CGO_ENABLED=1 go install github.com/projectdiscovery/katana/cmd/katana@latest
 
 # ExploitDB
